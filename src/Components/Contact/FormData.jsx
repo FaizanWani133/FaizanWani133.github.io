b1cac8efa73baff900e5e75d3d2b7ddd7a391080
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-11-14 00:24:33 +0530

    faizan

--- src/Components/Contact/FormData.jsx
+++ src/Components/Contact/FormData.jsx
@@ -6,19 +6,20 @@ function FormData() {
     
        
         <Box>
-            
+            <form  name='contact' method='post'>
             <FormControl borderRadius={"10px"} display={"flex"} flexDir="column" gap={"20px"}  p={4} border={"1px solid black"} margin={"50px auto"} width={{base:"80%",sm:"60%",lg:"50%",xl:"40%"}}>
             
             <Text fontSize={"25px"} fontWeight="500" textAlign={"center"}><span className='purpleText'>GET</span> IN TOUCH </Text>
-            <form  name='contact' method='post'>
-            <input type="hidden" name="form-name" value="the-name-of-the-html-form" />
+            
+            <input type="hidden" name="form-name" value="contact" />
                 <Input name='name' placeholder='Enter Full Name' type={"text"}></Input>
                 <Input name='email' placeholder='Enter Email Address' type={"email"}></Input>
                 <Input name='number' placeholder='Enter Phone Number' type={"number"}></Input>
                 <Textarea name='message' minH={"150px"} placeholder='Enter Your Message'></Textarea>
                 <Button type='submit' colorScheme={"red"}>Submit</Button>
-                </form>   
+                 
             </FormControl>
+            </form>  
             
         </Box>
 
