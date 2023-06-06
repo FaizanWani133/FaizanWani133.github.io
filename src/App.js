405ba2d22332d76685b548c74a96dc71e433df10
Fayzan Mukhtar <P7167244@nessinhyd.com>
2023-06-06 10:58:47 +0530

    import ReactGA from

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-
-import { Box, Heading } from '@chakra-ui/react';
-import './App.css';
-import About from './Components/AboutMe/About';
-import Contactme from './Components/Contact/Contactme';
-import Footer from './Components/footer/Footer';
-import Navbar from './Components/Navbar/Navbar';
-import Projects from './Components/Projects/Projects';
-import Skills from './Components/Skills/Skills';
-import Stats from './Components/Statistics/Stats';
+import { Box, Heading } from "@chakra-ui/react";
+import "./App.css";
+import About from "./Components/AboutMe/About";
+import Contactme from "./Components/Contact/Contactme";
+import Footer from "./Components/footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Projects from "./Components/Projects/Projects";
+import Skills from "./Components/Skills/Skills";
+import Stats from "./Components/Statistics/Stats";
+import ReactGA from "react-ga";
+const trackingId = "G-08JCD2XE0F";
+ReactGA.initialize(trackingId);
 
 function App() {
   return (
-    <Box  >
-      <Navbar/>
+    <Box>
+      <Navbar />
       <About />
-      <Heading id='Skills' bg={"transparent"} mt={"50px"} textAlign={"center"}><span className='purpleText'>S</span>KILLS</Heading>
-      <Skills/>
+      <Heading id="Skills" bg={"transparent"} mt={"50px"} textAlign={"center"}>
+        <span className="purpleText">S</span>KILLS
+      </Heading>
+      <Skills />
       <Projects />
-      <Stats/>
-      <Contactme/>
-      <Footer/>
-
+      <Stats />
+      <Contactme />
+      <Footer />
     </Box>
-    
   );
 }
 
--- src/Components/AboutMe/About.jsx
+++ src/Components/AboutMe/About.jsx
@@ -1,13 +1,22 @@
-import { Box, Button, Divider, Flex, Heading, Img, Link, Text, } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Img,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
+import ReactGA from "react-ga";
 
 function About() {
   return (
-    <Box  p={5} bg={"transparent"} id="About%20Me">
-        
+    <Box p={5} bg={"transparent"} id="About%20Me">
       <Flex
-      bg={"transparent"}
+        bg={"transparent"}
         margin={"0 auto"}
         width="80%"
         p={{ base: "10px", sm: "20px", lg: "25px", xl: "30px" }}
@@ -21,60 +30,91 @@ function About() {
         align="center"
       >
         <Box
-        bg={"transparent"}
+          bg={"transparent"}
           width={{ base: "100%", sm: "100%", lg: "50%", xl: "50%" }}
           textAlign={{ base: "center", sm: "center", lg: "left", xl: "left" }}
           lineHeight="30px"
         >
-          <Link _hover={{textDecoration:"none"}} fontSize={{base:"20px",sm:"25px",lg:"30px",xl:"30px"}} mb="20px" fontWeight="bold">
-          
-
-<Typewriter
-  options={{
-    strings: ['Hi , I am Fayzan Mukhtar'],
-    autoStart: true,
-    loop: true,
-  }}
-/>
-                
+          <Link
+            _hover={{ textDecoration: "none" }}
+            fontSize={{ base: "20px", sm: "25px", lg: "30px", xl: "30px" }}
+            mb="20px"
+            fontWeight="bold"
+          >
+            <Typewriter
+              options={{
+                strings: ["Hi , I am Fayzan Mukhtar"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </Link>
           <Text fontWeight={"500"}>
             I am a Full Stack Web Developer and i have knowledge in HTML, CSS,
             JS, REACT and i make user-friendly websites.
           </Text>
-          <Link _hover={{textDecoration:"none"}} href='https://drive.google.com/u/1/uc?id=1Gop4PetnFzT8rh_jaB7ag5TYymIXvw8q&export=download'>
-          <Button mt={"10px"} colorScheme="red">
-            Download CV
-          </Button>
+          <Link
+            _hover={{ textDecoration: "none" }}
+            href="https://drive.google.com/u/1/uc?id=1Gop4PetnFzT8rh_jaB7ag5TYymIXvw8q&export=download"
+          >
+            <Button
+              mt={"10px"}
+              colorScheme="red"
+              onClick={() =>
+                ReactGA.event({
+                  category: "CV download",
+                  action: "click event",
+                  label: "DOWNLOAD CV",
+                  value: 10,
+                })
+              }
+            >
+              Download CV
+            </Button>
           </Link>
         </Box>
         <Box width={{ base: "100%", sm: "100%", lg: "50%", xl: "50%" }}>
           <Img
-          alt="random image"
+            alt="random image"
             margin={"auto"}
-            width={{base:"150px",sm:"180px",lg:"200px",xl:"250px"}}
-            height={{base:"150px",sm:"180px",lg:"200px",xl:"250px"}}
+            width={{ base: "150px", sm: "180px", lg: "200px", xl: "250px" }}
+            height={{ base: "150px", sm: "180px", lg: "200px", xl: "250px" }}
             borderRadius={"50%"}
             objectFit="cover"
             src={require("./../../Images/profile-pic (4).png")}
           ></Img>
         </Box>
       </Flex>
-      <Box width={"100%"} p="4" mb={"20px"}><Heading textAlign={"center"}><span className='purpleText'>A</span>BOUT ME</Heading></Box>
-      
+      <Box width={"100%"} p="4" mb={"20px"}>
+        <Heading textAlign={"center"}>
+          <span className="purpleText">A</span>BOUT ME
+        </Heading>
+      </Box>
+
       <Flex
         flexDir={{ base: "column", sm: "column", lg: "row", xl: "row" }}
-        width={{base:"95%",sm:"90%",lg:"80%",xl:"80%"}}
-        
+        width={{ base: "95%", sm: "90%", lg: "80%", xl: "80%" }}
         margin={"0 auto"}
         p={4}
         gap="20px"
         boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px"
         borderRadius={"20px"}
       >
-        <Box lineHeight={"40px"} bg={"#D3E3FC"} borderRadius="10px" p="4" width={{base:"100%",sm:"100%",lg:"30%",xl:"30%"}} >
-          <Text mb={"10px"} fontSize={{base:"15px",sm:"18px",lg:"22px",xl:"22px"}} fontWeight="500">Personal Details</Text>
-          <Divider/>
+        <Box
+          lineHeight={"40px"}
+          bg={"#D3E3FC"}
+          borderRadius="10px"
+          p="4"
+          width={{ base: "100%", sm: "100%", lg: "30%", xl: "30%" }}
+        >
+          <Text
+            mb={"10px"}
+            fontSize={{ base: "15px", sm: "18px", lg: "22px", xl: "22px" }}
+            fontWeight="500"
+          >
+            Personal Details
+          </Text>
+          <Divider />
           <Text>
             <b>Email:</b> wfaizan133@gmail.com
           </Text>
@@ -85,15 +125,21 @@ function About() {
             <b>Nationality:</b> Indian
           </Text>
         </Box>
-        <Box width={{base:"100%",sm:"100%",lg:"70%",xl:"70%"}} p="4">
-          <Text mb={"10px"} fontSize={{base:"15px",sm:"18px",lg:"22px",xl:"22px"}} fontWeight="500">I am Full Stack Web Developer</Text>
+        <Box width={{ base: "100%", sm: "100%", lg: "70%", xl: "70%" }} p="4">
+          <Text
+            mb={"10px"}
+            fontSize={{ base: "15px", sm: "18px", lg: "22px", xl: "22px" }}
+            fontWeight="500"
+          >
+            I am Full Stack Web Developer
+          </Text>
           <Text lineHeight={"35px"}>
-            My name is Fayzan Wani, I am from Shopian, J&K.
-            Passionate Full Stack Web Developer with skills in the MERN stack, I
-            have learned Full Stack Web Development in Masai school. Skilled in
-            problem- solving and execution of software tasks from start to
-            finish. I look forward to joining a company where I can contribute
-            to individual and company growth..
+            My name is Fayzan Wani, I am from Shopian, J&K. Passionate Full
+            Stack Web Developer with skills in the MERN stack, I have learned
+            Full Stack Web Development in Masai school. Skilled in problem-
+            solving and execution of software tasks from start to finish. I look
+            forward to joining a company where I can contribute to individual
+            and company growth..
           </Text>
         </Box>
       </Flex>
